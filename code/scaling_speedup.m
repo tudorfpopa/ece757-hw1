70739213c25f963fc79a8c9ba5970ce7e936bad3
tudor popa <tpopa@wisc.edu>
2026-02-20 17:35:08 -0600

    add matlab script to compute scaling speedups

---
+++ code/scaling_speedup.m
@@ -0,0 +1,20 @@
+N = [1 2 4 8 16];
+content = fileread('../outputs/output.txt');
+total_time = regexp(content, 'Execution time in cycles: (\d+)', 'tokens');
+total_time = str2double(total_time{1}{1});
+parallel_time = regexp(content, 'Execution time of parallelizable section in cycles: (\d+)', 'tokens');
+parallel_time = str2double(parallel_time{1}{1});
+p = parallel_time / total_time;
+s = 1 - p;
+speedup = 1./(s+(p./N));
+plot(N, speedup, '-o');
+for i = 1:length(N)
+    text(N(i), speedup(i), sprintf('  (N=%d, %.2f)', N(i), speedup(i)), 'Color', 'black');
+end
+xlabel('Number of processors', 'Color', 'black');
+ylabel('Speedup', 'Color', 'black');
+title('Amdahl''s Law Speedup', 'Color', 'black', 'FontSize', 14);
+xlim([0 18]);
+set(gca, 'XColor', 'black', 'YColor', 'black', 'FontSize', 12);
+drawnow;
+print(gcf, '../outputs/speedup_plot.png', '-dpng', '-r300');
