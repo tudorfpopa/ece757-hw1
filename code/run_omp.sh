bd9f8d795cb6c9562fc1f5b6caaf3d9d1411bef8
tudor popa <tpopa@wisc.edu>
2026-02-20 21:22:38 -0600

    first attempt at parallelizing using omp

---
+++ code/run_omp.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# clean and compile
+rm -rf ./kymppitonni
+gcc -o kymppitonni kymppitonni-pthreads.c -fopenmp
+
+RUNS=10
+
+for THREADS in 2 3 4 5 6 7 8 9 10 11 12; do
+    OUTPUT=./output_pthreads_${THREADS}.txt
+    touch $OUTPUT
+    TOTAL=0
+    for i in $(seq 1 $RUNS); do
+        RESULT=$(./kymppitonni 5000 10 300 $THREADS | grep "Execution time in cycles")
+        CYCLES=$(echo $RESULT | grep -oP '\d+')
+        TOTAL=$((TOTAL + CYCLES))
+    done
+    AVG=$((TOTAL / RUNS))
+    echo "Threads: $THREADS, Average execution time: $AVG cycles" | tee $OUTPUT
+done
+
+# clean and move outputs
+rm -rf ./kymppitonni
+for THREADS in 2 3 4 5 6 7 8 9 10 11 12; do
+    rm -rf ../outputs/output_pthreads_${THREADS}.txt
+    mv ./output_pthreads_${THREADS}.txt ../outputs/output_pthreads_${THREADS}.txt
+done
